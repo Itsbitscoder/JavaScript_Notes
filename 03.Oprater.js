3cd1972470b1813d62012e2f7560040cc6a460c2
Kunal Jadhav <154059626+Itsbitscoder@users.noreply.github.com>
2024-06-23 23:58:28 +0530

    03.Oprater added

---
+++ 03.Oprater.js
@@ -0,0 +1,66 @@
+let score = "Kunal"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Kunal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+// "" => false
+// "Kunal" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negVal = -value
+//console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Kunal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);  // => 1
+// console.log(+"");    // => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++; //=> profix
+++gameCounter; //=> prifix
+console.log(gameCounter);
